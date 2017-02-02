51ce74853d489bf4e8c31565bd9284c88ba19448
Octavian Dospinescu <doctav@uaic.ro>
2017-02-02 16:35:09 +0200

    FrmSender

---
+++ app/src/main/java/com/example/user/appintents/FrmSender.java
@@ -0,0 +1,13 @@
+package com.example.user.appintents;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FrmSender extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_sender);
+    }
+}
